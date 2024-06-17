ee54738a4be976d4b450ec1be7f34f084e39c325
ali laptop <ali.hastam2015@gmail.com>
2024-06-18 01:57:34 +0330

    vercel

--- app/(src)/layout/layout.tsx
+++ app/(src)/layout/layout.tsx
@@ -1,8 +1,7 @@
 import React, { ReactElement } from 'react';
-import { LayoutProps } from '@/.next/types/app/layout';
 import Header from '../componenets/Header';
 
-export default function layout({ children }: LayoutProps): ReactElement {
+export default function layout({ children }: any): ReactElement {
   return (
     <>
       <Header />
